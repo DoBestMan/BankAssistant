2686048761dd535bbf3972f96c74c95758f60e61
doBestMan <Do.best.Man@outlook.com>
2023-05-09 11:06:14 -0400

    scroll view get started page

--- src/Containers/HomeContainer.js
+++ src/Containers/HomeContainer.js
@@ -11,67 +11,71 @@ import { useTheme } from '@/Hooks'
 import { Brand } from '@/Components'
 import { setDefaultTheme } from '@/Store/Theme'
 import { navigate } from '@/Navigators/utils'
-
+import { ScrollView } from 'react-native-gesture-handler'
 const HomeContainer = () => {
   const { Layout, Images, Fonts, Colors, Gutters } = useTheme()
 
   return (
-    <View style={[Layout.fill, styles.background]}>
-      <Image
-        style={styles.backgroundImg}
-        source={Images.home}
-        resizeMode="stretch"
-      />
-
-      <View style={[Layout.fill, styles.container]}>
+    <ScrollView
+        style={styles.scrollView}
+      >
+      <View style={[Layout.fill, styles.background]}>
         <Image
-          style={Gutters.smallVMargin}
-          source={Images.logo_full}
-          resizeMode="contain"
+          style={styles.backgroundImg}
+          source={Images.home}
+          resizeMode="stretch"
         />
-        <Text style={[Fonts.textLarge, styles.title]}>Your best local</Text>
-        <Text style={[Fonts.textLarge, styles.title]}>mobile laundry</Text>
-        <Text style={[Fonts.textLarge, styles.title]}>service</Text>
-        <Text style={[Fonts.textSmall, styles.text, { color: Colors.white }]}>
-          Let us take a load off! Serving Tulsa, OK and surrounding areas Fresh
-          Fabrics is a wash, dry, fold service that picks up and drops off at
-          your desired location. Returning all items same day!
-        </Text>
 
-        <View
-          style={[
-            Layout.fill,
-            Layout.justifyContentCenter,
-            Gutters.largeBMargin,
-          ]}
-        >
-          <TouchableOpacity
-            style={[styles.startBtn, Layout.center]}
-            onPress={() => navigate('CustomerOnboarding')}
-          >
-            <Text style={[Fonts.textRegular, styles.startBtnText]}>
-              Get started
-            </Text>
-          </TouchableOpacity>
+        <View style={[Layout.fill, styles.container]}>
+          <Image
+            style={Gutters.smallVMargin}
+            source={Images.logo_full}
+            resizeMode="contain"
+          />
+          <Text style={[Fonts.textLarge, styles.title]}>Your best local</Text>
+          <Text style={[Fonts.textLarge, styles.title]}>mobile laundry</Text>
+          <Text style={[Fonts.textLarge, styles.title]}>service</Text>
+          <Text style={[Fonts.textSmall, styles.text, { color: Colors.white }]}>
+            Let us take a load off! Serving Tulsa, OK and surrounding areas Fresh
+            Fabrics is a wash, dry, fold service that picks up and drops off at
+            your desired location. Returning all items same day!
+          </Text>
 
-          <TouchableOpacity
-            style={[styles.loginBtn, Gutters.regularTMargin, Layout.center]}
-            onPress={() => navigate('Login')}
+          <View
+            style={[
+              Layout.fill,
+              Layout.justifyContentCenter,
+              Gutters.largeBMargin,
+            ]}
           >
-            <Text style={[Fonts.textRegular, styles.loginBtnText]}>Log in</Text>
-          </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.startBtn, Layout.center]}
+              onPress={() => navigate('CustomerOnboarding')}
+            >
+              <Text style={[Fonts.textRegular, styles.startBtnText]}>
+                Get started
+              </Text>
+            </TouchableOpacity>
 
-          <TouchableOpacity
-            style={[Gutters.regularTMargin, Layout.center]}
-            onPress={() => navigate('FreshieOnboarding')}
-          >
-            <Text style={[Fonts.textRegular, styles.startBtnText]}>
-              {'Become a Freshie >'}
-            </Text>
-          </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.loginBtn, Gutters.regularTMargin, Layout.center]}
+              onPress={() => navigate('Login')}
+            >
+              <Text style={[Fonts.textRegular, styles.loginBtnText]}>Log in</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={[Gutters.regularTMargin, Layout.center]}
+              onPress={() => navigate('FreshieOnboarding')}
+            >
+              <Text style={[Fonts.textRegular, styles.startBtnText]}>
+                {'Become a Freshie >'}
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
-    </View>
+    </ScrollView>
   )
 }
 

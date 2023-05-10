c02001e896ec04ec7807e197511f1b27f7d82d03
doBestMan <Do.best.Man@outlook.com>
2023-05-10 09:11:46 -0400

    fix: order support

--- src/Components/OrderSupportCom.js
+++ src/Components/OrderSupportCom.js
@@ -10,74 +10,145 @@ import {
 import { useTheme } from '@/Hooks'
 import { ScrollView } from 'react-native-gesture-handler'
 
-const TITLES = ['Order online', 'Wash. Dry. Fold.', 'Deliver on time']
-const DESCRIPTIONS = [
-  'Place your order. Feel free to provide any special instructions and we will pick it up.',
-  'We will wash, dry & fold your items and follow any special instructions',
-  'We return your laundry the same day we pick up to your desired location.',
+const MENU_ITEMS = [
+  [ 'Ordering', "What's included?", 'Is there a minimum?', 'Do you service my area?' ],
+  [ 'Preparing for pick', 'Do I put my clothes out for pick up in laundry bags?', 'What days do you pick up and deliver?', 'Do I have to serparate lights and darks?' ],
+  [ 'Care of clothes', 'Do you launder clothes in cold wather or hot?', 'Do you use fabric softener' ]
 ]
 
 const OrderSupportCom = ({ onClose }) => {
-  const { Layout, Images, Gutters } = useTheme()
-  const [currentStep, setCurrentStep] = useState(1)
+  const { Layout, Images, Gutters, Fonts } = useTheme()
+  const [question, setQuestion] = useState('')
 
-  const images = [Images.howItWorks1, Images.howItWorks2, Images.howItWorks3]
-
-  const onNext = () => {
-    if (currentStep < 3) {
-      setCurrentStep(currentStep + 1)
-    } else {
-      onClose()
-    }
+  const onClickSubtitle = (question) => {
+    setQuestion(question);
   }
 
+  const onBack = () => {
+    if(question) setQuestion('');
+    else onClose();
+  }
+  
   return (
     <ScrollView>
       <SafeAreaView style={styles.wrapper}>
         <View style={[styles.header, Layout.row, Layout.alignItemsCenter]}>
-          <TouchableOpacity style={styles.closeButton} onPress={onClose}>
-            <Image source={Images.close} resizeMode="contain" />
+          <TouchableOpacity style={styles.closeButton} onPress={onBack}>
+            <Image source={Images.left_arrow} resizeMode="contain" />
           </TouchableOpacity>
-          <Text style={styles.title}>Order Support</Text>
-        </View>
-
-        <View style={[Layout.fill]}>
-          <View
-            style={[
-              styles.imageWrapper,
-              Layout.fill,
-              Layout.alignItemsCenter,
-              Layout.justifyContentCenter,
-            ]}
-          >
-            <Image source={images[currentStep - 1]} resizeMode="contain" />
-          </View>
-        <View style={[Layout.alignItemsCenter, styles.orderOnline]}>
-            <View style={styles.stepNumberWrapper}>
-              <Text style={styles.stepNumber}>{currentStep}</Text>
-            </View>
-            <Text style={styles.stepTitle}>{TITLES[currentStep - 1]}</Text>
-            <Text style={styles.description}>
-              {DESCRIPTIONS[currentStep - 1]}
-            </Text>
-            <TouchableOpacity style={styles.button} onPress={onNext}>
-              <Text style={styles.buttonText}>Next</Text>
+          { !question &&
+            <TouchableOpacity
+              style={[styles.notification, Layout.right]}
+            >
+              <Image
+                style={styles.bellIcon}
+                source={Images.notification_empty}
+                resizeMode="stretch"
+              />
             </TouchableOpacity>
-            <View style={[Layout.row, Gutters.regularBMargin]}>
-              {Array(3)
-                .fill()
-                .map((value, index) => (
-                  <TouchableOpacity
-                    key={index}
-                    style={[
-                      styles.stepDot,
-                      index === currentStep - 1 && styles.dotSelected,
-                    ]}
-                  />
-                ))}
+          }
+        </View>
+        { !question && 
+          <View style={styles.wrapper}>
+            <Text style={[Fonts.textLarge, styles.title]}>Order Support</Text>
+            <View style={Gutters.largeTMargin}>
+              <Text style={[ styles.subtitle]}>Select your issue or question</Text>
+              <TouchableOpacity
+                  key={"When will my laundry be picked up/ dropped off?"}
+                  style={styles.item}
+                  onPress={() => onClickSubtitle("When will my laundry be picked up/ dropped off?")}
+                >
+                <Text style={styles.itemText}>{"When will my laundry be picked up/ dropped off?"}</Text>
+                <Image source={Images.right_blue_arrow} resizeMode="contain" />
+              </TouchableOpacity>
+              
+              <TouchableOpacity
+                  key={"How much does my laundry weight? What is the cost?"}
+                  style={styles.item}
+                  onPress={() => onClickSubtitle("How much does my laundry weight? What is the cost?")}
+                >
+                <Text style={styles.itemText}>{"How much does my laundry weight? What is the cost?"}</Text>
+                <Image source={Images.right_blue_arrow} resizeMode="contain" />
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                  key={"Do I need to separate my clothes?"}
+                  style={styles.item}
+                  onPress={() => onClickSubtitle("Do I need to separate my clothes?")}
+                >
+                <Text style={styles.itemText}>{"Do I need to separate my clothes?"}</Text>
+                <Image source={Images.right_blue_arrow} resizeMode="contain" />
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                  key={"What is included in my order?"}
+                  style={styles.item}
+                  onPress={() => onClickSubtitle("What is included in my order?")}
+                >
+                <Text style={styles.itemText}>{"What is included in my order?"}</Text>
+                <Image source={Images.right_blue_arrow} resizeMode="contain" />
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                  key={"Cancelled order"}
+                  style={styles.item}
+                  onPress={() => onClickSubtitle("Cancelled order")}
+                >
+                <Text style={styles.itemText}>{"Cancelled order"}</Text>
+                <Image source={Images.right_blue_arrow} resizeMode="contain" />
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                  key={"My freshie did not arrive"}
+                  style={styles.item}
+                  onPress={() => onClickSubtitle("My freshie did not arrive")}
+                >
+                <Text style={styles.itemText}>{"My freshie did not arrive"}</Text>
+                <Image source={Images.right_blue_arrow} resizeMode="contain" />
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                  key={"My laundry was not delivered"}
+                  style={styles.item}
+                  onPress={() => onClickSubtitle("My laundry was not delivered")}
+                >
+                <Text style={styles.itemText}>{"My laundry was not delivered"}</Text>
+                <Image source={Images.right_blue_arrow} resizeMode="contain" />
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                  key={"Why my order may be canceled?"}
+                  style={styles.item}
+                  onPress={() => onClickSubtitle("Why my order may be canceled?")}
+                >
+                <Text style={styles.itemText}>{"Why my order may be canceled?"}</Text>
+                <Image source={Images.right_blue_arrow} resizeMode="contain" />
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                  key={"Do you pick up in my neighborhood?"}
+                  style={styles.item}
+                  onPress={() => onClickSubtitle("Do you pick up in my neighborhood?")}
+                >
+                <Text style={styles.itemText}>{"Do you pick up in my neighborhood?"}</Text>
+                <Image source={Images.right_blue_arrow} resizeMode="contain" />
+              </TouchableOpacity>
+
             </View>
           </View>
-        </View>
+        }
+        { question && 
+          <View style={styles.wrapper}>
+            <Text style={[styles.subtitle, styles.question]}>{question}</Text>
+            <Text style={[styles.itemText, styles.itemPg]}>FAQ article goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet tempor lacus eu sollicitudin. Nunc a diam in ligula mattis fringilla. Nulla congue felis nec ex luctus, vitae elementum diam venenatis. Quisque libero ligula, pretium at aliquam id, vestibulum nec massa. Nulla quam massa, tristique quis gravida a, vulputate a tortor. Suspendisse potenti. Morbi placerat aliquam vestibulum. Praesent cursus pellentesque felis, et condimentum tellus fringilla eu.
+</Text>
+            <Text style={[ styles.subtitle]}>Subtitle goes here</Text>
+            <Text style={[styles.itemText, styles.itemPg]}>FAQ article goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet tempor lacus eu sollicitudin. Nunc a diam in ligula mattis fringilla. Nulla congue felis nec ex luctus, vitae elementum diam venenatis. Quisque libero ligula, pretium at ulputate a tortor. Suspendisse potenti. Morbi placerat aliquam vestibulum. Praesent cursus pellentesque felis, et condimentum tellus fringilla eu.
+
+Nulla congue felis nec ex luctus, vitae elementum diam venenatis. Quisque libero ligula, pretium at ulputate a tortor. Suspendisse potenti. 
+</Text>
+          </View>
+        }
       </SafeAreaView>
     </ScrollView>
   )
@@ -92,15 +163,36 @@ const styles = StyleSheet.create({
     height: '100%',
     backgroundColor: 'white',
   },
+  question: {
+    fontSize: 30
+  },
+  itemPg: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  title: {
+    fontFamily: 'Program OT',
+    fontWeight: '700',
+    color: '#43C3EF'
+  },
   header: {
     position: 'relative',
     height: 30,
     justifyContent: 'center',
     marginTop: 10,
   },
-  title: {
-    color: '#0E0E0E',
-    fontSize: 18,
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomColor: '#E2E2E2',
+    borderBottomWidth: 2,
+    height: 60,
+  },
+  subtitle: {
+    color: '#43C3EF',
+    fontSize: 16,
+    fontWeight: '500',
   },
   closeButton: {
     position: 'absolute',
@@ -133,6 +225,10 @@ const styles = StyleSheet.create({
     color: '#43C3EF',
     fontSize: 16,
     fontWeight: '700',
+  },
+  notification: {
+    marginLeft: 'auto',
+    marginRight: 30,
   },
   stepTitle: {
     color: '#43C3EF',

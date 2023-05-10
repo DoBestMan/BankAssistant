0e834908d941ab3c7451a4b8f89de4ece89dd807
doBestMan <Do.best.Man@outlook.com>
2023-05-10 09:06:59 -0400

    fix: faq page

--- src/Components/FaqCom.js
+++ src/Components/FaqCom.js
@@ -18,116 +18,128 @@ const MENU_ITEMS = [
 
 const FaqCom = ({ onClose }) => {
   const { Layout, Images, Gutters, Fonts } = useTheme()
-  const [currentStep, setCurrentStep] = useState(1)
-
-  const onNext = () => {
-    if (currentStep < 3) {
-      setCurrentStep(currentStep + 1)
-    } else {
-      onClose()
-    }
-  }
+  const [question, setQuestion] = useState('')
 
   const onClickSubtitle = (question) => {
-    
-    console.log(20230509,'onCLick');
+    setQuestion(question);
   }
 
+  const onBack = () => {
+    if(question) setQuestion('');
+    else onClose();
+  }
+  
   return (
     <ScrollView>
       <SafeAreaView style={styles.wrapper}>
         <View style={[styles.header, Layout.row, Layout.alignItemsCenter]}>
-          <TouchableOpacity style={styles.closeButton} onPress={onClose}>
+          <TouchableOpacity style={styles.closeButton} onPress={onBack}>
             <Image source={Images.left_arrow} resizeMode="contain" />
           </TouchableOpacity>
-          <TouchableOpacity
-            style={[styles.notification, Layout.right]}
-          >
-            <Image
-              style={styles.bellIcon}
-              source={Images.notification_empty}
-              resizeMode="stretch"
-            />
-          </TouchableOpacity>
+          { !question &&
+            <TouchableOpacity
+              style={[styles.notification, Layout.right]}
+            >
+              <Image
+                style={styles.bellIcon}
+                source={Images.notification_empty}
+                resizeMode="stretch"
+              />
+            </TouchableOpacity>
+          }
         </View>
-        <View style={styles.wrapper}>
-          <Text style={[Fonts.textLarge, styles.title]}>FAQ</Text>
-          <View style={Gutters.largeTMargin}>
-            <Text style={[ styles.subtitle]}>Ordering</Text>
-            <TouchableOpacity
-                key={"What's included?"}
-                style={styles.item}
-                onPress={() => onClickSubtitle("What's included?")}
-              >
-              <Text style={styles.itemText}>{"What's included?"}</Text>
-              <Image source={Images.right_blue_arrow} resizeMode="contain" />
-            </TouchableOpacity>
-            <TouchableOpacity
-                key={'Is there a minimum?'}
-                style={styles.item}
-                onPress={() => onClickSubtitle('Is there a minimum?')}
-              >
-              <Text style={styles.itemText}>{'Is there a minimum?'}</Text>
-              <Image source={Images.right_blue_arrow} resizeMode="contain" />
-            </TouchableOpacity>
-            <TouchableOpacity
-                key={'Do you service my area?'}
-                style={styles.item}
-                onPress={() => onClickSubtitle('Do you service my area?')}
-              >
-              <Text style={styles.itemText}>{'Do you service my area?'}</Text>
-              <Image source={Images.right_blue_arrow} resizeMode="contain" />
-            </TouchableOpacity>
-          </View>
+        { !question && 
+          <View style={styles.wrapper}>
+            <Text style={[Fonts.textLarge, styles.title]}>FAQ</Text>
+            <View style={Gutters.largeTMargin}>
+              <Text style={[ styles.subtitle]}>Ordering</Text>
+              <TouchableOpacity
+                  key={"What's included?"}
+                  style={styles.item}
+                  onPress={() => onClickSubtitle("What's included?")}
+                >
+                <Text style={styles.itemText}>{"What's included?"}</Text>
+                <Image source={Images.right_blue_arrow} resizeMode="contain" />
+              </TouchableOpacity>
+              <TouchableOpacity
+                  key={'Is there a minimum?'}
+                  style={styles.item}
+                  onPress={() => onClickSubtitle('Is there a minimum?')}
+                >
+                <Text style={styles.itemText}>{'Is there a minimum?'}</Text>
+                <Image source={Images.right_blue_arrow} resizeMode="contain" />
+              </TouchableOpacity>
+              <TouchableOpacity
+                  key={'Do you service my area?'}
+                  style={styles.item}
+                  onPress={() => onClickSubtitle('Do you service my area?')}
+                >
+                <Text style={styles.itemText}>{'Do you service my area?'}</Text>
+                <Image source={Images.right_blue_arrow} resizeMode="contain" />
+              </TouchableOpacity>
+            </View>
 
-          <View style={Gutters.largeTMargin}>
-            <Text style={[ styles.subtitle]}>Preparing for pick</Text>
-            <TouchableOpacity
-                key={"Do I put my clothes out for pick up in laundry bags?"}
-                style={styles.item}
-                onPress={() => onClickSubtitle("Do I put my clothes out for pick up in laundry bags?")}
-              >
-              <Text style={styles.itemText}>{"Do I put my clothes out for pick up in laundry bags?"}</Text>
-              <Image source={Images.right_blue_arrow} resizeMode="contain" />
-            </TouchableOpacity>
-            <TouchableOpacity
-                key={'What days do you pick up and deliver?'}
-                style={styles.item}
-                onPress={() => onClickSubtitle('What days do you pick up and deliver?')}
-              >
-              <Text style={styles.itemText}>{'What days do you pick up and deliver?'}</Text>
-              <Image source={Images.right_blue_arrow} resizeMode="contain" />
-            </TouchableOpacity>
-            <TouchableOpacity
-                key={'Do I have to serparate lights and darks?'}
-                style={styles.item}
-                onPress={() => onClickSubtitle('Do I have to serparate lights and darks?')}
-              >
-              <Text style={styles.itemText}>{'Do I have to serparate lights and darks?'}</Text>
-              <Image source={Images.right_blue_arrow} resizeMode="contain" />
-            </TouchableOpacity>
-          </View>
+            <View style={Gutters.largeTMargin}>
+              <Text style={[ styles.subtitle]}>Preparing for pick</Text>
+              <TouchableOpacity
+                  key={"Do I put my clothes out for pick up in laundry bags?"}
+                  style={styles.item}
+                  onPress={() => onClickSubtitle("Do I put my clothes out for pick up in laundry bags?")}
+                >
+                <Text style={styles.itemText}>{"Do I put my clothes out for pick up in laundry bags?"}</Text>
+                <Image source={Images.right_blue_arrow} resizeMode="contain" />
+              </TouchableOpacity>
+              <TouchableOpacity
+                  key={'What days do you pick up and deliver?'}
+                  style={styles.item}
+                  onPress={() => onClickSubtitle('What days do you pick up and deliver?')}
+                >
+                <Text style={styles.itemText}>{'What days do you pick up and deliver?'}</Text>
+                <Image source={Images.right_blue_arrow} resizeMode="contain" />
+              </TouchableOpacity>
+              <TouchableOpacity
+                  key={'Do I have to serparate lights and darks?'}
+                  style={styles.item}
+                  onPress={() => onClickSubtitle('Do I have to serparate lights and darks?')}
+                >
+                <Text style={styles.itemText}>{'Do I have to serparate lights and darks?'}</Text>
+                <Image source={Images.right_blue_arrow} resizeMode="contain" />
+              </TouchableOpacity>
+            </View>
 
-          <View style={Gutters.largeTMargin}>
-            <Text style={[ styles.subtitle]}>Care of clothes</Text>
-            <TouchableOpacity
-                key={"Do you launder clothes in cold wather or hot?"}
-                style={styles.item}
-                onPress={() => onClickSubtitle("Do you launder clothes in cold wather or hot?")}
-              >
-              <Text style={styles.itemText}>{"Do you launder clothes in cold wather or hot?"}</Text>
-              <Image source={Images.right_blue_arrow} resizeMode="contain" />
-            </TouchableOpacity>
-            <TouchableOpacity
-                key={'Do you use fabric softener'}
-                style={styles.item}
-                onPress={() => onClickSubtitle('Do you use fabric softener')}
-              >
-              <Text style={styles.itemText}>{'Do you use fabric softener'}</Text>
-              <Image source={Images.right_blue_arrow} resizeMode="contain" />
-            </TouchableOpacity>
+            <View style={Gutters.largeTMargin}>
+              <Text style={[ styles.subtitle]}>Care of clothes</Text>
+              <TouchableOpacity
+                  key={"Do you launder clothes in cold wather or hot?"}
+                  style={styles.item}
+                  onPress={() => onClickSubtitle("Do you launder clothes in cold wather or hot?")}
+                >
+                <Text style={styles.itemText}>{"Do you launder clothes in cold wather or hot?"}</Text>
+                <Image source={Images.right_blue_arrow} resizeMode="contain" />
+              </TouchableOpacity>
+              <TouchableOpacity
+                  key={'Do you use fabric softener'}
+                  style={styles.item}
+                  onPress={() => onClickSubtitle('Do you use fabric softener')}
+                >
+                <Text style={styles.itemText}>{'Do you use fabric softener'}</Text>
+                <Image source={Images.right_blue_arrow} resizeMode="contain" />
+              </TouchableOpacity>
+            </View>
           </View>
-        </View>
+        }
+        { question && 
+          <View style={styles.wrapper}>
+            <Text style={[styles.subtitle, styles.question]}>{question}</Text>
+            <Text style={[styles.itemText, styles.itemPg]}>FAQ article goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet tempor lacus eu sollicitudin. Nunc a diam in ligula mattis fringilla. Nulla congue felis nec ex luctus, vitae elementum diam venenatis. Quisque libero ligula, pretium at aliquam id, vestibulum nec massa. Nulla quam massa, tristique quis gravida a, vulputate a tortor. Suspendisse potenti. Morbi placerat aliquam vestibulum. Praesent cursus pellentesque felis, et condimentum tellus fringilla eu.
+</Text>
+            <Text style={[ styles.subtitle]}>Subtitle goes here</Text>
+            <Text style={[styles.itemText, styles.itemPg]}>FAQ article goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet tempor lacus eu sollicitudin. Nunc a diam in ligula mattis fringilla. Nulla congue felis nec ex luctus, vitae elementum diam venenatis. Quisque libero ligula, pretium at ulputate a tortor. Suspendisse potenti. Morbi placerat aliquam vestibulum. Praesent cursus pellentesque felis, et condimentum tellus fringilla eu.
+
+Nulla congue felis nec ex luctus, vitae elementum diam venenatis. Quisque libero ligula, pretium at ulputate a tortor. Suspendisse potenti. 
+</Text>
+          </View>
+        }
       </SafeAreaView>
     </ScrollView>
   )
@@ -141,6 +153,13 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     backgroundColor: 'white',
+  },
+  question: {
+    fontSize: 30
+  },
+  itemPg: {
+    marginTop: 10,
+    marginBottom: 10,
   },
   title: {
     fontFamily: 'Program OT',
